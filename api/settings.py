9135271dcec6a67f5d18bef1020e28444d7e310d
dharanish <dharanishwar.2006@gmail.com>
2026-01-27 18:47:14 -1200

    production error fix

--- api/settings.py
+++ api/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*','http://localhost:3000','127.0.0.1', '.vercel.app','e-comm-ivory-six.vercel.app']
+ALLOWED_HOSTS = ['*','http://localhost:3000','127.0.0.1', '.vercel.app','e-comm-ivory-six.vercel.app','.onrender.com',]
 
 
 # Application definition
