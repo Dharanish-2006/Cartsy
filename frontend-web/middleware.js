0b4f3931685802fb30c22cc411ed23ba40407b9a
Dharanish-2006 <dharanishwar.2006@gmail.com>
2026-01-30 18:48:30 +0530

    path fix

--- frontend-web/middleware.js
+++ frontend-web/middleware.js
@@ -3,7 +3,16 @@ import { NextResponse } from "next/server";
 export function middleware(request) {
   const token = request.cookies.get("access")?.value;
   const { pathname } = request.nextUrl;
+
   const publicRoutes = ["/login", "/signup", "/verify-otp"];
+
+  if (
+    pathname.startsWith("/_next") ||
+    pathname.startsWith("/favicon") ||
+    pathname.startsWith("/api")
+  ) {
+    return NextResponse.next();
+  }
 
   if (!token && !publicRoutes.includes(pathname)) {
     return NextResponse.redirect(new URL("/login", request.url));
