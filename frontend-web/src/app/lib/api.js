d602979d880baaf9b2d7aa013767a5f4cc5eecae
Dharanish <130727582+Dharanish-2006@users.noreply.github.com>
2026-02-01 00:53:34 -1200

    Update api.js

--- frontend-web/src/app/lib/api.js
+++ frontend-web/src/app/lib/api.js
@@ -2,7 +2,14 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://cartsy-ht0x.onrender.com/api/",
-  withCredentials: true,
+});
+
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("access");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
 });
 
 export default api;
