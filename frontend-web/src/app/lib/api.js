63de423ae522fe62c325e3ccdf67b950dbe36956
Dharanish-2006 <dharanishwar.2006@gmail.com>
2026-02-01 12:46:01 +0530

    login api / cookie fix

--- frontend-web/src/app/lib/api.js
+++ frontend-web/src/app/lib/api.js
@@ -1,79 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cartsy-ht0x.onrender.com/api",
+  baseURL: "https://cartsy-ht0x.onrender.com/api/",
+  withCredentials: true,
 });
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("access");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
-
-let isRefreshing = false;
-let failedQueue = [];
-
-const processQueue = (error, token = null) => {
-  failedQueue.forEach((prom) => {
-    if (error) {
-      prom.reject(error);
-    } else {
-      prom.resolve(token);
-    }
-  });
-  failedQueue = [];
-};
-
-api.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const originalRequest = error.config;
-
-    if (
-      error.response?.status === 401 &&
-      !originalRequest._retry
-    ) {
-      if (isRefreshing) {
-        return new Promise((resolve, reject) => {
-          failedQueue.push({ resolve, reject });
-        }).then((token) => {
-          originalRequest.headers.Authorization = "Bearer " + token;
-          return api(originalRequest);
-        });
-      }
-
-      originalRequest._retry = true;
-      isRefreshing = true;
-
-      try {
-        const refresh = localStorage.getItem("refresh");
-
-        const res = await axios.post(
-          "https://cartsy-ht0x.onrender.com/api/token/refresh/",
-          { refresh }
-        );
-
-        localStorage.setItem("access", res.data.access);
-        api.defaults.headers.Authorization =
-          "Bearer " + res.data.access;
-
-        processQueue(null, res.data.access);
-
-        return api(originalRequest);
-      } catch (err) {
-        processQueue(err, null);
-        localStorage.clear();
-        window.location.href = "/login";
-        return Promise.reject(err);
-      } finally {
-        isRefreshing = false;
-      }
-    }
-
-    return Promise.reject(error);
-  }
-);
 
 export default api;
