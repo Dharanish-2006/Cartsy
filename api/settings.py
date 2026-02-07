6cbda5361166f3cc99367fffc8482a1e5e2a16d6
Dharanish-2006 <dharanishwar.2006@gmail.com>
2026-02-07 17:25:44 +0530

    cors update

--- api/settings.py
+++ api/settings.py
@@ -93,6 +93,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "https://cartsy-rho.vercel.app",
+    "https://cartsy-frontend.onrender.com/",
 ]
 
 REST_FRAMEWORK = {
