91325bc3de26fe5d3b4b478f0e65c885b4d8453e
dharanish <dharanishwar.2006@gmail.com>
2026-02-14 16:11:06 +0530

    auth method change

--- authentication/views.py
+++ authentication/views.py
@@ -1,19 +1,19 @@
 from django.core.mail import send_mail
 from django.conf import settings
-from django.contrib.auth import authenticate,logout
+from django.contrib.auth import authenticate
+from django.utils import timezone
+from datetime import timedelta
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
 
 from .models import EmailOTP, User
 from OrderManagement.utils.otp import generate_otp
-from django.utils import timezone
-from datetime import timedelta
 
-from rest_framework.decorators import api_view, permission_classes
-from rest_framework.permissions import IsAuthenticated, AllowAny
 
 class SignupAPI(APIView):
     def post(self, request):
@@ -55,7 +55,10 @@ class SignupAPI(APIView):
             fail_silently=False,
         )
 
-        return Response({"message": "OTP sent successfully"})
+        return Response(
+            {"message": "OTP sent successfully"},
+            status=status.HTTP_200_OK
+        )
 
 
 class LoginAPI(APIView):
@@ -92,32 +95,14 @@ class LoginAPI(APIView):
 
         refresh = RefreshToken.for_user(user)
 
-        response = Response(
-            {"message": "Login successful"},
+        return Response(
+            {
+                "access": str(refresh.access_token),
+                "refresh": str(refresh),
+                "message": "Login successful"
+            },
             status=status.HTTP_200_OK
         )
-
-        response.set_cookie(
-            key="access",
-            value=str(refresh.access_token),
-            httponly=True,
-            secure=True,
-            samesite="None",
-            # domain=".onrender.com",
-            max_age=60 * 60
-        )
-
-        response.set_cookie(
-            key="refresh",
-            value=str(refresh),
-            httponly=True,
-            secure=True,
-            samesite="None",
-            # domain=".onrender.com",
-            max_age=60 * 60 * 24 * 7
-        )
-
-        return response
 
 
 class VerifyOTP(APIView):
@@ -126,40 +111,47 @@ class VerifyOTP(APIView):
         entered_otp = request.data.get("otp")
 
         if not email or not entered_otp:
-            return Response({"error": "Email and OTP required"}, status=400)
+            return Response(
+                {"error": "Email and OTP required"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         try:
             user = User.objects.get(email=email)
             otp_obj = EmailOTP.objects.get(user=user)
         except (User.DoesNotExist, EmailOTP.DoesNotExist):
-            return Response({"error": "Invalid request"}, status=400)
+            return Response(
+                {"error": "Invalid request"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         if timezone.now() - otp_obj.created_at > timedelta(minutes=5):
             otp_obj.delete()
-            return Response({"error": "OTP expired"}, status=400)
+            return Response(
+                {"error": "OTP expired"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         if otp_obj.otp != entered_otp:
-            return Response({"error": "Invalid OTP"}, status=400)
+            return Response(
+                {"error": "Invalid OTP"},
+                status=status.HTTP_400_BAD_REQUEST
+            )
 
         user.is_active = True
         user.save()
         otp_obj.delete()
 
-        return Response({"message": "Account verified successfully"})
+        return Response(
+            {"message": "Account verified successfully"},
+            status=status.HTTP_200_OK
+        )
 
 
 @api_view(["POST"])
 @permission_classes([IsAuthenticated])
 def LogoutAPI(request):
-    logout(request)
-
-    response = Response(
+    return Response(
         {"message": "Logged out successfully"},
-        status=200
+        status=status.HTTP_200_OK
     )
-
-    response.delete_cookie("access", path="/")
-    response.delete_cookie("refresh", path="/")
-    response.delete_cookie("csrftoken", path="/")
-
-    return response
