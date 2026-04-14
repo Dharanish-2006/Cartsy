e4a28e4508bc1ec04e08dde88dc38c505ace0737
dharanish <dharanishwar.2006@gmail.com>
2026-04-14 09:54:34 +0530

    cors

--- Inventory/urls.py
+++ Inventory/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
 from .views import *
 urlpatterns = [
-    path("api/home/", PublicHomeAPI),
-    path("api/products/<int:pk>/", PublicProductDetailAPI),
-    path("api/cart/", CartAPI.as_view()),
-    path("api/cart/update/", UpdateCartQuantity.as_view()),
-    path("api/orders/", OrdersAPI.as_view()),
-    path("api/orders/create/", CreateOrderAPI.as_view()),
-    path("api/orders/razorpay/", CreateRazorpayOrderAPI.as_view()),
-    path("api/orders/razorpay/verify/", VerifyPaymentAPI.as_view()),
-    path("ping/", ping),
+    path("api/home/",                    HomeAPI),
+    path("api/products/<int:pk>/",       ProductDetailAPI.as_view()),
+    path("api/cart/",                    CartAPI.as_view()),
+    path("api/cart/update/",             UpdateCartQuantity.as_view()),
+    path("api/orders/",                  OrdersAPI.as_view()),
+    path("api/orders/create/",           CreateOrderAPI.as_view()),
+    path("api/orders/razorpay/",         CreateRazorpayOrderAPI.as_view()),
+    path("api/orders/razorpay/verify/",  VerifyPaymentAPI.as_view()),
+    path("ping/",                        ping),
 ]
