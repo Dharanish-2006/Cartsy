1fa9fa2f608b35e7d7cd86cf0ed739be790304a9
dharanish <dharanishwar.2006@gmail.com>
2026-04-23 06:15:57 +0530

    ui update

--- api/asgi.py
+++ api/asgi.py
@@ -1,18 +1,11 @@
 import os
-from django.core.asgi import get_asgi_application
+import django
 from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.auth import AuthMiddlewareStack
-from api.routing import websocket_urlpatterns
-from OrderManagement.middleware import JWTAuthMiddleware  # 👈 ADD THIS
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+django.setup()
+from OrderManagement.middleware import JWTAuthMiddleware
+from django.core.asgi import get_asgi_application
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
-
-    "websocket": JWTAuthMiddleware(
-        AuthMiddlewareStack(
-            URLRouter(websocket_urlpatterns)
-        )
-    ),
 })
