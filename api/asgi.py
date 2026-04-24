809ddffc2a911ee3ad0abab4f2af7a3975cb7f83
dharanish <dharanishwar.2006@gmail.com>
2026-04-25 01:20:54 +0530

    svs

--- api/asgi.py
+++ api/asgi.py
@@ -1,5 +1,7 @@
 import os
 import django
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+django.setup()
 
 from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
@@ -7,8 +9,6 @@ from django.core.asgi import get_asgi_application
 from api.routing import websocket_urlpatterns
 from authentication.jwt_ws_middleware import JWTAuthMiddleware
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
-django.setup()
 
 django_asgi_app = get_asgi_application()
 
